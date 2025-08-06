194980845599174cb04983ca6e68c89c76b76d8e
54ntu <chaudharysantaram8@gmail.com>
2025-08-06 14:25:00 +0545

    committed to new_1 branch

--- test.js
+++ test.js
@@ -5,3 +5,12 @@ console.log("this one is for master branch.....")
 
 
 console.log("this one is committed to new_1")
+
+
+
+const test=()=>{
+    console.log("this one is the latest commit")
+}
+
+
+test()
